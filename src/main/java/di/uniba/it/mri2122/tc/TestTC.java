8d51187462f12c90d7e47dc392a76dab61be17bb
pierpaolo <pierpaolo@pierpaolo-microhal>
2021-12-01 18:28:13 +0100

    - Text categorization

---
+++ src/main/java/di/uniba/it/mri2122/tc/TestTC.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package di.uniba.it.mri2122.tc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author pierpaolo
+ */
+public class TestTC {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        // load data
+        DatasetReader tr = new CSVDatasetReader();
+        List<DatasetExample> traingset = tr.getExamples(new File("resources/TC/train.csv"));
+        DatasetReader ts = new CSVDatasetReader();
+        List<DatasetExample> testset = ts.getExamples(new File("resources/TC/test.csv"));
+       
+        // Dummy       
+        TextCategorization dummy = new DummyClassifier();
+        dummy.train(traingset);
+        List<String> pdummy = dummy.test(testset);
+        System.out.println("Dummy accuracy: " + dummy.accuracy(testset, pdummy));
+        dummy.evaluate(testset, pdummy);
+        // KNN        
+        TextCategorization knn = new KNN(13);
+        knn.train(traingset);
+        List<String> pknn = knn.test(testset);
+        System.out.println("KNN accuracy: " + knn.accuracy(testset, pknn));
+        knn.evaluate(testset, pknn);
+    }
+
+}
